4e367ffca37c375cd8de570fe25834905c3e73a8
KristalliDev <kristal.dev@proton.me>
2023-01-15 08:50:56 +0000

    yeet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <style>
 body {
-	background-color: rgb(47, 56, 76)}
+	/* background-color: rgb(47, 56, 76); */
+	color: rgb(47, 56, 76);
+}
 
 h1 {
-	display: flex;
 	font-family: monospace;
 	font-size: 13vh;
-	color: rgb(222, 222, 222);
-	justify-content: center;
 }
 </style>
 
 <body>
-	<h1>Hello, Sailors :D</h1>
+	<section>
+	<h1 style="display: flex; justify-content: center;">Hello, Sailors :D</h1>
+	</section>
 </body>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<style>
+    h1 {
+        color: rgb(47, 56, 76);
+        font-family: monospace;
+        font-size: 13vh;
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
+<h1>
+    About
+</h1>
